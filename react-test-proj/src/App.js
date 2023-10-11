8f9edb94b33dc89713df0b5edf8ca6ad7ebd4639
trentlr <67702832+trentlr@users.noreply.github.com>
2023-10-11 10:34:03 -0400

    async test for user events

--- react-test-proj/src/App.js
+++ react-test-proj/src/App.js
@@ -1,9 +1,18 @@
+import {useState, useEffect} from "react";
+
 
 export default function App() {
     
+    const [heading, setHeading] = useState("my-test-app");
+
+    const handleClick = () => {
+        setHeading((prevHeading) => "a test-app project")
+    }
+
     return(
-        <h1>
-            my-test-app
-        </h1>
+        <div>
+        <h1> {heading} </h1>
+        <button role="button" onClick={handleClick}> Update Heading </button>
+        </div>
     )
 }
--- react-test-proj/src/App.test.js
+++ react-test-proj/src/App.test.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-test("does app render", () => {
-    render(<App/>)
-    expect(screen.getByRole("heading", { level: 1})).toHaveTextContent(/my-test-app/i);
-})
+describe("App component", () => {
+    test("my-test-app heading", () => {
+        render(<App />)
+        expect(screen.getByRole("heading")).toHaveTextContent(/my-test-app/i)
+    });
+
+    test("a my-test-app subheading", async () => {
+        render(<App />)
+
+        const button = screen.getByRole("button", {name: "Update Heading"})
+
+        await userEvent.click(button)
+
+        expect(screen.getByRole("heading")).toHaveTextContent(/a test-app project/i)
+    });
+});
